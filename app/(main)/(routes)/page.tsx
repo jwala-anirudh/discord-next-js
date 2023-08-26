3af8d45642c7f9b272a4269a6c4339ac173edec8
Anirudh Jwala <anirudhsai2@gmail.com>
2023-08-26 18:27:02 +0530

    Structuring the application code

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+const Home = () => {
+  return (
+    <div>
+      <p className='text-3xl font-bold text-indigo-500'>Hello discord</p>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default Home;
